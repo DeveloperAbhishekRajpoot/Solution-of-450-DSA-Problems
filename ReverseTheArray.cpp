34e0c26641522bbd1e57f34298dad32bb1c28281
DeveloperAbhishekRajpoot <dev.abhishek.rajpoot@gmail.com>
2024-08-23 22:42:44 +0530

    Create ReverseTheArray.cpp

---
+++ ReverseTheArray.cpp
@@ -0,0 +1,30 @@
+// INTUTION => i will swao the fist val with the last one and move forward the front pointer and backbard the rear pointer .
+
+/* Time complexity  => O(n)
+   Space Complexity => O(1) 
+*/
+
+#include<iostream> 
+using namespace std;
+
+// function to reverse an Array in (In-Place) with constant extra space 
+
+void reverseArray( int arr[] , int size ){
+  if( size == 0)
+    return ;
+  int front = 0 , rear = size - 1;
+  while( front < rear ){
+    swap(arr[front] , arr[rear]);
+    front++ ; rear-- ;
+  }
+  
+}
+int main(){
+  int arr[8] = { 1 , 2 , 3 , 4 , 5 , 6 , 7 , 8 };
+  reverseArray( arr , 8);
+  cout<<" The arry after reversal "<<endl;
+  for( int i = 0 ; i < 8 ; i++ ){
+    cout<<arr[i]<<" ";
+  } 
+  return 0;
+}
