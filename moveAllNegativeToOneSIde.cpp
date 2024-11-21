3d71f6315bdcddcbf2b0a16e849ae054e195d1af
DeveloperAbhishekRajpoot <dev.abhishek.rajpoot@gmail.com>
2024-11-21 14:08:37 +0530

    solve the problem using two pointer approch

---
+++ moveAllNegativeToOneSIde.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+void moveNegativesToOneSide(vector<int> &arr){
+    int n = arr.size();
+    int i = 0, j = 0;
+
+    while( i < n){
+        if( arr[i] < 0 ){
+            swap(arr[i] , arr[j]);
+            i++; j++;
+        }
+        else{
+            i++;
+        }
+    }
+}
+
+int main(){
+    vector<int> arr = {-12,3,-4,6,5,3,5,-5,-10,-3,2};
+    moveNegativesToOneSide(arr);
+
+    for( auto  it : arr){
+        cout<<it<<" ";
+    }
+    return 0;
+}
